070f69f41bb9362b8941bbe046c5a6b33e75a647
Alan Loreto Cornidez <aloretocornidez@arizona.edu>
2023-09-19 20:03:30 -0700

    auto-commit

---
+++ lecture/function-pass-by-pointer/sample-code/activity-01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int *ptr, i = 10, j = 20, k = 30;
+
+  ptr = &i;
+
+  *ptr = 40;
+
+  ptr = &j;
+
+  // *ptr = *ptr + I;
+  *ptr += i;
+
+  ptr = &k;
+
+  *ptr = *ptr + k + j;
+
+  printf("i = %d, j = %d, k = %d", i, j, *ptr);
+
+  return (0);
+}
